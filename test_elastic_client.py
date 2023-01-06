b34676f878eacd1f7ea7107bde0f03b3378b1698
Andy Kohler <akohler@library.ucla.edu>
2023-01-06 12:08:59 -0900

    SYS-1125:/Update fields to match target index

--- test_elastic_client.py
+++ test_elastic_client.py
@@ -4,7 +4,12 @@ from elastic_lib_client import ElasticLibClient
 def main():
     es = ElasticLibClient("http://localhost:9200")
     es.index_libguides()
+    # For testing, since indexing can still be running when search is done
+    es.ELASTIC_SEARCH.indices.refresh()
+    document_count = es.ELASTIC_SEARCH.count()["count"]
+    print(f"{document_count = }")
     resp = es.ELASTIC_SEARCH.search(query={"match_all": {}})
+    print("Showing first (up to) 10 titles...")
     for hit in resp["hits"]["hits"]:
         print(hit["_source"]["title"])
 
